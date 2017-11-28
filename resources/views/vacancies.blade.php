941577de50051f3c25702a12cf150f655e4f3380
Kristaps Ozols <ozols1992@hotmail.com>
2017-11-28 14:27:24 +0100

    edit vacancies blade

--- resources/views/vacancies.blade.php
+++ resources/views/vacancies.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     @if (Auth::check())
       <h1>Job vacancies</h1>
+      
     @else
       <h3>You need to log in. <a href="/login">Click here to login</a></h3>
     @endif
