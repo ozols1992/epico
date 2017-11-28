ebb74866e92e6f183692411f3f9237c5f34a45bf
Kristaps Ozols <ozols1992@hotmail.com>
2017-11-28 13:21:40 +0100

    callapi function update

--- app/Http/Controllers/epicoApiController.php
+++ app/Http/Controllers/epicoApiController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+require 'vendor/autoload.php';
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
@@ -10,6 +11,20 @@ class epicoApiController extends Controller
     //
     public function callapi()
     {
-      
+      $client = new \GuzzleHttp\Client();
+      $res = $client->request('GET', 'http://epico.dk/umbraco/surface/home/AllAdvertising');
+      echo $res->getStatusCode();
+      // 200
+      echo $res->getHeaderLine('content-type');
+      // 'application/json; charset=utf8'
+      echo $res->getBody();
+      // '{"id": 1420053, "name": "guzzle", ...}'
+
+      // Send an asynchronous request.
+      $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
+      $promise = $client->sendAsync($request)->then(function ($response) {
+          echo 'I completed! ' . $response->getBody();
+      });
+      $promise->wait();
     }
 }
