bdd7aebe4472ba7c7d06245d5e2fb96333307743
Kristaps Ozols <ozols1992@hotmail.com>
2017-11-28 13:59:11 +0100

    some stuff

--- app/Http/Controllers/epicoApiController.php
+++ app/Http/Controllers/epicoApiController.php
@@ -1,30 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
-require 'vendor/autoload.php';
+
+use GuzzleHttp\Client as GuzzleHttpClient;
+use GuzzleHttp\Exception\RequestException;
 
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
 
 class epicoApiController extends Controller
 {
     //
     public function callapi()
-    {
-      $client = new \GuzzleHttp\Client();
-      $res = $client->request('GET', 'http://epico.dk/umbraco/surface/home/AllAdvertising');
-      echo $res->getStatusCode();
-      // 200
-      echo $res->getHeaderLine('content-type');
-      // 'application/json; charset=utf8'
-      echo $res->getBody();
-      // '{"id": 1420053, "name": "guzzle", ...}'
+{
+      try {
 
-      // Send an asynchronous request.
-      $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
-      $promise = $client->sendAsync($request)->then(function ($response) {
-          echo 'I completed! ' . $response->getBody();
-      });
-      $promise->wait();
-    }
+           $client = new GuzzleHttpClient();
+
+           $apiRequest = $client->request('GET', 'http://epico.dk/umbraco/surface/home/AllAdvertising');
+
+          $content = json_decode($apiRequest->getBody()->getContents());
+
+      } catch (RequestException $re) {
+
+      }
+      return view('vacancies');
+ }
+
 }
