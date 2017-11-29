ca7adbad751d8a821414e5c459f565055744a1bb
Kristaps Ozols <ozols1992@hotmail.com>
2017-11-29 14:39:24 +0100

    epico API works!!!

--- app/Http/Controllers/epicoApiController.php
+++ app/Http/Controllers/epicoApiController.php
@@ -1,12 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Http\Request as httpRequest ;
-use App\Product;
-use App\Http\Requests;
-use GuzzleHttp\Client;
-use GuzzleHttp\Message\Request;
-use GuzzleHttp\Message\Response;
+
+use Illuminate\Http\Request;
+
 
 class epicoApiController extends Controller
 {
@@ -15,6 +12,7 @@ class epicoApiController extends Controller
     {
       $json = file_get_contents('http://epico.dk/umbraco/surface/home/AllAdvertising');
       $obj = json_decode($json);
+
       return view('vacancies', compact('obj'));
     }
 
--- resources/views/vacancies.blade.php
+++ resources/views/vacancies.blade.php
@@ -3,13 +3,30 @@
 @section('content')
 <div class="container">
     @if (Auth::check())
-      <h1>Job vacancies</h1>
-              @foreach ($obj as $job)
-
-              <h3 class='jobtitle'>{{ $job->HeadLine }}</h3>
-              
-
-              @endforeach
+      <h1>Job vacancies</h1><br>
+        @foreach ($obj as $job)
+          <h3 class='jobtitle'>{{ $job->HeadLine }}</h3>
+        @if ($job->AdvertisingType == "Fast")
+          <p class='advertisingtype'>Looking for permanent employee</p>
+        @endif
+        @if ($job->AdvertisingType == "Freelance")
+          <p class='advertisingtype'>Looking for freelancer</p>
+        @endif
+        @if ($job->Location !== null)
+          <p class='location'>{{ $job->Location }}</p>
+        @endif
+          <p class='country'>{{ $job->Country }}</p>
+          <p class='description'>{{ $job->Description }}</p>
+          <p class='begindate'>Job start: {{ $job->JobBeginDate }}</p>
+          <p class='deadline'>Application deadline: {{ $job->Applicationdeadline }}</p>
+        @if ($job->Duration !== null)
+          <p class='duration'>Job duration: {{ $job->Duration }}</p>
+        @endif
+          <p class='contactjob'>Contact: {{ $job->SearchEmail }}</p>
+        @if ($job->Footer !== null)
+          <p class='jobfooter'>{{ $job->Footer }}</p>
+        @endif
+        @endforeach
     @else
       <h3>You need to log in. <a href="/login">Click here to login</a></h3>
     @endif
