027e4fb82d9b736ae91850aa204344cd62f73814
Kristaps Ozols <ozols1992@hotmail.com>
2017-12-01 13:40:46 +0100

    news back in business

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,25 @@
 @section('content')
 <div class="container">
     @if (Auth::check())
+
+    @foreach ($xml->NewsAll->News  as $news)
+
+      <h3>{{ $news->attributes()->headline }}</h3>
+      <h4>{{ $news->attributes()->contactEmail }}</h4>
+      <h4>{{ $news->attributes()->publishDate }}</h4>
+      <div>{{ $news->div->div->div->div->asXML() }}</div>
+
+
+    @endforeach
+
     <h2>Welcome to EPICO</h2>
     @else
     <div class="starttop">
         <img class="startlogo" src="img/For_dark_&_red_background.png" alt=""/>
-        
+
             <a class="startlogin" href="/register">Register</a>
-            <a class="startlogin" href="/login">Sign in</a>            
-        
+            <a class="startlogin" href="/login">Sign in</a>
+
     </div>
     <div class="startmid">
         <span class="fa fa-chevron-down"></span>
