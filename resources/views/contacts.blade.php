3f05ca424a3e7caca0b7781d99ae62a3b0e234de
Kristaps Ozols <ozols1992@hotmail.com>
2017-12-04 14:07:56 +0100

    contacts

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -4,11 +4,17 @@
 <div class="container">
     <h2>EPICO news</h2><br>
 
-    @foreach ($xml->ContactList as $contacts)
+    @foreach ($xml->ContactList->Contact as $contacts)
+    {!! $contacts->attributes()->image !!}
+    <h4>{!! $contacts->attributes()->name !!}</h4>
+    <h5>{!! $contacts->attributes()->phone !!}</h5>
+    <h5>{!! $contacts->attributes()->email !!}</h5>
+    <h5>{!! $contacts->attributes()->department !!}</h5>
+    <h5>{!! $contacts->attributes()->location !!}</h5>
 
 
     @endforeach
 
-    
+
 </div>
 @endsection
